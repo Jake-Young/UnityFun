fd4d986258923c4a46cf05107d9275255fd8400a
Jake Young <steroidcrazygun@gmail.com>
2020-04-01 16:55:15 +0100

    UnityXR Interaction and Teleport Basics

---
+++ Assets/Scripts/OculusVRTK/OVRInputButtonAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zinnia.Action;
+
+public class OVRInputButtonAction : BooleanAction
+{
+    public OVRInput.Controller m_Controller = OVRInput.Controller.Active;
+    public OVRInput.Button m_Button;
+
+    private void Update()
+    {
+        Receive(OVRInput.Get(m_Button, m_Controller));
+    }
+}
---
+++ Assets/Scripts/OculusVRTK/OVRInputTouchAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zinnia.Action;
+
+public class OVRInputTouchAction : BooleanAction
+{
+    public OVRInput.Controller m_Controller = OVRInput.Controller.Active;
+    public OVRInput.Touch m_Touch;
+
+    private void Update()
+    {
+        Receive(OVRInput.Get(m_Touch, m_Controller));
+    }
+}
